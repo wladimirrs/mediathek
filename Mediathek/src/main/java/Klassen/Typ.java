43a31b47bc5b193790f405c3fcf639d0bbfedba4
wladimirrs <185645045+wladimirrs@users.noreply.github.com>
2026-06-16 13:49:11 +0200

    Klasse Typ hinzugefügt

--- Mediathek/src/main/java/Klassen/Typ.java
+++ Mediathek/src/main/java/Klassen/Typ.java
@@ -6,7 +6,34 @@ public class Typ {
     String bezeichnung;
 
 
-    
+    public Typ(int id, String bezeichnung) {    // Konstruktoren
+        this.id = id;
+        this.bezeichnung = bezeichnung;
+    }
+
+    public Typ(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public Typ() {}
 
 
+
+
+
+    public int getId() {    // getter und setter
+        return id;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
 }
